7529047738e7ff50eecf555d16e412e6ff42de89
Moro-Code <omardev9898@gmail.com>
2020-02-27 14:02:58 -0500

    test suite for categories table

--- src/api/src/database/database.e2e.spec.ts
+++ src/api/src/database/database.e2e.spec.ts
@@ -200,6 +200,11 @@ describe("Database Schema Tests", () => {
                     "INSERT INTO links ( url, title, language, description ) VALUES ('https://test1.com', 'test', 'en', 'an entry for a test')"
                 )
             })
+            it('image_link field exists', async () => {
+                await client.query(
+                    "INSERT INTO links ( url, title, language, image_link ) VALUES ('https://test1.com', 'test', 'en', 'an image link' ) "
+                )
+            })
         })
         
 
@@ -341,6 +346,258 @@ describe("Database Schema Tests", () => {
             })
 
         })
+
         
+    })
+
+    describe("categories table tests" , () => {
+        describe("fields table tests", () => {
+            it ("should insert valid data with title and language", async () => {
+                await client.query(
+                    "INSERT INTO categories ( title, language ) VALUES ( 'test category', 'en' )"
+                )
+
+                let results =  await client.query(
+                    "SELECT * FROM categories"
+                )
+
+                expect(results.rowCount).toBe(1)
+                expect(results.rows[0]["title"]).toBe("test category")
+                expect(results.rows[0]["language"]).toBe("en")
+            })
+
+            it("has a self incrementing primary key", async () => {
+                await client.query(
+                    `
+                    INSERT INTO categories (title, language) VALUES ('test1', 'en');
+                    INSERT INTO categories (title, language) VALUES ('test2', 'en');
+                    INSERT INTO categories (title, language) VALUES ('test3', 'en');
+                    `
+                )
+
+                let results = await client.query(
+                    "SELECT * FROM categories "
+                )
+
+                expect(results.rowCount).toBe(3)
+
+                for (let i in results.rows){
+                    expect(results.rows[i]["id"]).toBe(parseInt(i) + 1)
+                }
+            })
+            it("should fail on not including language in insert", async () => {
+                let errorMessage:string = ""
+                try {
+                    await client.query(
+                        `INSERT INTO categories (title) VALUES ('test')`
+                    )
+                }
+                catch(e){
+                    errorMessage = e.stack
+                }
+                expect(errorMessage).toMatch(/error: null value in column "language" violates not-null constraint/)
+            })
+            it("should fail on not including title in insert", async () => {
+                let errorMessage:string = ""
+                try {
+                    await client.query(
+                        `INSERT INTO categories (language) VALUES ('en')`
+                    )
+                }
+                catch(e){
+                    errorMessage = e.stack
+                }
+                expect(errorMessage).toMatch(/error: null value in column "title" violates not-null constraint/)
+                
+            })
+            it('should allow "en" and "fr" values for language column', async () => {
+                await client.query(
+                    `INSERT INTO categories (title, language ) VALUES ( 'test-en', 'en' );
+                     INSERT INTO categories (title, language ) VALUES ( 'test-fr', 'fr' );
+                    `
+                )
+            })
+    
+            it ('should allow no other values for language column', async () => {
+                let randValue = Math.random().toString(36).slice(7)
+                let errorMessage:string = ""
+                try {
+                    await client.query(
+                        " INSERT INTO categories (title, language) VALUES ('test', $1::text ) ",
+                        [
+                            randValue
+                        ]
+                    )
+                }catch(e){
+                    errorMessage = e.stack
+                }
+                expect(errorMessage).toMatch(/error: new row for relation "categories" violates check constraint "language_english_or_french"/)
+            })
+
+            it ("title field should be unique", async () => {
+                let errorMessage:string = ""
+                try {
+                    await client.query(
+                        `
+                        INSERT INTO categories ( title, language ) VALUES ('test', 'en');
+                        INSERT INTO categories ( title, language ) VALUES ('test', 'en');
+                        `
+                    )
+                }
+                catch(e){
+                    errorMessage = e.stack
+                }
+                expect(errorMessage).toMatch(/error: duplicate key value violates unique constraint "categories_title_key"/)
+            })
+            it ("description field exists", async () => {
+                await client.query(
+                    "INSERT INTO categories ( title, language, description ) VALUES ('test', 'en', 'an entry for a test')"
+                )
+            })
+
+            it("image_link fiels exists", async () => {
+                await client.query(
+                    "INSERT INTO categories ( title, language, image_link ) VALUES ('test', 'en', 'link_to_image') "
+                )
+            })
+        })
+        describe("trigger tests", () => {
+            it("creates a row in the logs table on insert", async () => {
+                await client.query(
+                    "INSERT INTO categories (language, title ) VALUES ( 'en', 'test' )"
+                )
+                
+
+                let results = await client.query(
+                    "SELECT * FROM logs"
+                )
+
+                expect(results.rowCount).toBe(1)
+                
+                let action = await client.query(
+                    'SELECT action_name FROM actions WHERE id = $1::integer ',
+                    [
+                        results.rows[0]["action_id"]
+                    ]
+                )
+
+                expect(action.rowCount).toBe(1)
+                expect(action.rows[0]["action_name"]).toBe("CREATE_CATEGORY")
+                expect(results.rows[0]["category"]).toBe("test")
+            })
+
+            it("creates a row in the logs table on update", async () => {
+                await client.query(
+                    "INSERT INTO categories (language, title ) VALUES ('en', 'test')"
+                )
+                await client.query(
+                    "UPDATE categories SET title = 'test_updated' WHERE id = 1"
+                )
+
+                let results = await client.query(
+                    "SELECT * FROM logs ORDER BY created_on ASC"
+                )
+
+                expect(results.rowCount).toBe(2)
+
+                let action = await client.query(
+                    'SELECT action_name FROM actions WHERE id = $1::integer',
+                    [
+                        results.rows[1]["action_id"]
+                    ]
+                )
+                expect(action.rowCount).toBe(1)
+                expect(action.rows[0]["action_name"]).toBe("UPDATE_CATEGORY")
+                expect(results.rows[0]["category"]).toBe("test")
+                expect(results.rows[1]["category"]).toBe("test_updated")
+            })
+            it("creates a row in the logs table on delete", async () => {
+                await client.query(
+                    "INSERT INTO categories (language, title ) VALUES ('en', 'test')"
+                )
+                await client.query(
+                    "DELETE FROM categories WHERE id = 1"
+                )
+
+                let results = await client.query(
+                    "SELECT * FROM logs ORDER BY created_on ASC"
+                )
+
+                expect(results.rowCount).toBe(2)
+
+                let action = await client.query(
+                    'SELECT action_name FROM actions WHERE id = $1::integer',
+                    [
+                        results.rows[1]["action_id"]
+                    ]
+                )
+
+                expect(action.rowCount).toBe(1)
+                expect(action.rows[0]["action_name"]).toBe("DELETE_CATEGORY")
+                expect(results.rows[0]["category"]).toBe("test")
+                expect(results.rows[1]["category"]).toBe("test")
+            })
+
+            it("processes english text from title and description row on insert", async () => {
+                await client.query(
+                    "INSERT INTO categories (language, title, description) VALUES('en', 'test', 'this is a good site on testing' )"
+                )
+
+                let results = await client.query(
+                    "SELECT * FROM categories"
+                )
+                let manual_results = await client.query(
+                    "SELECT * FROM to_tsvector('en', 'test this is a good site on testing')"
+                )
+                
+                expect(results.rows[0]["ftx_data"]).toBe(manual_results.rows[0]["to_tsvector"])
+            })
+
+            it("processes english text from only title on row insert", async () => {
+                await client.query(
+                    "INSERT INTO categories (language, title ) VALUES ('en', 'this is an awesome test')"
+                )
+
+                let results = await client.query(
+                    "SELECT * FROM categories"
+                )
+                let manual_results = await client.query(
+                    "SELECT * FROM to_tsvector('en', 'this is an awesome test')"
+                )
+
+                expect(results.rows[0]["ftx_data"]).toBe(manual_results.rows[0]["to_tsvector"])
+            })
+
+            it("processes french text from title and description", async () => {
+                await client.query(
+                    "INSERT INTO categories (language, title, description) VALUES ('fr', 'bon site de test', 'c''est un site de test génial')"
+                )
+
+                let results = await client.query(
+                    "SELECT * FROM categories"
+                )
+                let manual_results = await client.query(
+                    "SELECT * FROM to_tsvector('fr', 'bon site de test c''est un site de test génial')"
+                )
+
+                expect(results.rows[0]["ftx_data"]).toBe(manual_results.rows[0]["to_tsvector"])
+            })
+
+            it("processes french text from only title on row insert", async () => {
+                await client.query(
+                    "INSERT INTO categories ( language, title ) VALUES ('fr', 'bon site de test')"
+                )
+
+                let results = await client.query(
+                    "SELECT * FROM categories"
+                )
+                let manual_results = await client.query(
+                    "SELECT * FROM to_tsvector('fr', 'bon site de test')"
+                )
+
+                expect(results.rows[0]["ftx_data"]).toBe(manual_results.rows[0]["to_tsvector"])
+            })
+
+        })
     })
 })
