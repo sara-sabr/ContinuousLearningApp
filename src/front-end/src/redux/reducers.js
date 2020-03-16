19b1ecce334774644a1a2779c21fbac669e3e541
Moro-Code <omardev9898@gmail.com>
2020-03-15 23:21:44 -0400

    reducer for links

--- src/front-end/src/redux/reducers.js
+++ src/front-end/src/redux/reducers.js
@@ -1,6 +1,7 @@
-import { combineReducers } from "redux"
-import {TYPES, LANGUAGES } from "./actions"
+import { combineReducers, bindActionCreators } from "redux"
+import {TYPES, RESOURCE_TYPES } from "./actions"
 import i18n from "../translations" 
+import { act } from "react-dom/test-utils"
 
 
 export const language = function(state = i18n.language, action){
@@ -12,9 +13,80 @@ export const language = function(state = i18n.language, action){
     }
 }
 
+export const links = function(state = {
+    isFetching: false,
+    fetchFailed: false,
+    data: {}
+}, action){
+    switch (action.type){
+        case TYPES.REQUEST:
+            switch (action.resourceType){
+                case RESOURCE_TYPES.LINKS:
+                    return {
+                        ...state,
+                        isFetching: true,
+                        previousQueryParams: {
+                            order: action.order,
+                            limit: action.limit,
+                            offset: action.offset
+                        }
+                    }
+                default:
+                    return state
+            }
+        case TYPES.RECIEVE:
+            switch (action.resourceType){
+                case RESOURCE_TYPES.LINKS:
+                    let currentLinks = Object.assign({}, state.data)
+                    let fetchedLinks = {}
+                    action.data.map(
+                        (val) => {
+                            fetchedLinks[val.id] = val
+                        }
+                    )
 
+                    if (Object.keys(currentLinks).length >= 1){
+                        fetchedLinks = {
+                            ...currentLinks,
+                            ...fetchedLinks
+                        }
+                    }
+                    else{
+                        let newState = {
+                            ...state,
+                            isFetching: false,
+                            fetchFailed: false,
+                            failureReason: undefined,
+                            failureMessage: undefined,
+                            data: fetchedLinks
+                        }
+
+                        return newState
+                    }
+                    
+                default:
+                    return state
+            }
+        case TYPES.REQUEST_FAILED:
+            switch(action.resourceType){
+                case RESOURCE_TYPES.LINKS:
+                    return {
+                        ...state,
+                        isFetching: false,
+                        fetchFailed: true,
+                        failureReason: action.failureReason,
+                        failureMessage: action.message
+                    }
+                default:
+                    return state
+            }
+        default:
+            return state
+    }
+} 
 const rootReducer = combineReducers(
     {
+        links,
         language
     }
 )
