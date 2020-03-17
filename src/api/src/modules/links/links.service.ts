3e13b9eacbcaf8dc79253c4f5cb5d0eb680cbd5f
Moro-Code <omardev9898@gmail.com>
2020-03-17 17:06:36 -0400

    added get by url route

--- src/api/src/modules/links/links.service.ts
+++ src/api/src/modules/links/links.service.ts
@@ -8,9 +8,7 @@ import { DatabaseError, NoDataFound } from "../../utils/errors"
 
 @Injectable()
 export class LinksService{
-    constructor(private readonly databaseService: DatabaseService){
-
-    }
+    constructor(private readonly databaseService: DatabaseService){}
     
     async getLinkById(id: string): Promise<ReturnedLinkDTO> {
         let parsedId = parseInt(id)
@@ -33,6 +31,40 @@ export class LinksService{
                 updatedOn: dbResult.updatedOn
             }
 
+            return returnedData
+        }catch(e){
+            if ( e instanceof DatabaseError){
+                throw new HttpException(
+                    "A database error has occured: " + e.message,
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                )
+            }
+            if ( e instanceof NoDataFound ){
+                throw new HttpException(
+                    "Resource not found: " + e.message,
+                    HttpStatus.NOT_FOUND
+                )
+            }
+            throw new HttpException(
+                "An unknown error has occured",
+                HttpStatus.INTERNAL_SERVER_ERROR 
+            )
+        }
+    }
+
+    async getLinkByURL(url: string): Promise<ReturnedLinkDTO>{
+        /* needs tests */
+        try{
+            let dbResult = await this.databaseService.readLinkByURL(url)
+            let returnedData: ReturnedLinkDTO = {
+                id: dbResult.id,
+                language: dbResult.language,
+                url: dbResult.url,
+                title: dbResult.title,
+                ...dbResult,
+                createdOn: dbResult.createdOn,
+                updatedOn: dbResult.updatedOn
+            }
             return returnedData
         }catch(e){
             if ( e instanceof DatabaseError){
