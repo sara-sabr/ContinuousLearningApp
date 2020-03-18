f1652508cefc1c5a30b0049d84d3e7ae4d8565f8
Eric Wu <e-wu@users.noreply.github.com>
2020-03-18 10:47:32 -0400

    Keycloak starter (#9)
    * Add .env to gitignore
    
    * Add example .env file
    
    * Static reference to keycloak to avoid dep ver
    
    * Add routes
    
    * keycloak init
    
    * Force login

--- src/front-end/src/App.js
+++ src/front-end/src/App.js
@@ -2,26 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import i18n from "./translations";
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Home from "./components/Home";
+import About from "./components/About";
+import Dashboard from "./components/Dashboard";
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/dashboard">Dashbord</Link></li>
+        </ul>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/dashboard" component={Dashboard} />
+      </div>
+    </BrowserRouter>
   );
 }
 
